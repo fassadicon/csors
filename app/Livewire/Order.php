1906220c45c75a1edb9d3c831c499b086b2123c8
snarf <fa.sadicon@gmail.com>
2024-09-24 21:51:36 +0800

    base date validation

--- app/Livewire/Order.php
+++ app/Livewire/Order.php
@@ -2,16 +2,23 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
 use App\Models\Caterer;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Order extends Component
 {
+
     public $cart = [];
     public $caterer;
 
+    #[Validate('required|date|before:endDateTime')]
     public $startDateTime;
+
+    #[Validate('required|date|after:startDateTime')]
     public $endDateTime;
+
     public $location;
     public $remarks;
     public float $totalAmount;
@@ -27,6 +34,10 @@ class Order extends Component
         })->sum('price');
     }
 
+    public function pay()
+    {
+        dd(Carbon::parse($this->startDateTime)->format('Y-m-d H:i:s'), $this->endDateTime, $this->location, $this->remarks);
+    }
     public function render()
     {
         // dd($this->cart);
--- resources/views/livewire/order.blade.php
+++ resources/views/livewire/order.blade.php
@@ -7,89 +7,84 @@
         <x-mary-button label="Add more items"
             class="btn-secondary" />
     </a>
-    <form wire:submit="checkout">
-        @foreach ($cart as $categoryName => $categoryItems)
-            <div>
-                <x-mary-header title="{{ ucwords($categoryName) }}"
-                    class="!my-2"
-                    size="text-xl"
-                    separator />
-                @foreach ($categoryItems as $key => $categoryItem)
-                    <x-mary-list-item :item="$categoryItem"
-                        no-separator
-                        no-hover>
-                        <x-slot:avatar>
-                            <x-mary-avatar :image="asset('images/placeholder.jpg')" />
-                        </x-slot:avatar>
-                        <x-slot:value>
-                            {{ $categoryName == 'foods' ? $categoryItem['orderItem']->foodDetail->name . ' - ' . $categoryItem['orderItem']->servingType->name : $categoryItem['orderItem']->name }}
-                        </x-slot:value>
-                        <x-slot:sub-value>
-                            @if ($categoryName == 'foods')
-                                PHP {{ $categoryItem['price'] }} for
-                                {{ $categoryItem['quantity'] }} pax
-                            @else
-                                PHP {{ $categoryItem['price'] }} for
-                                {{ $categoryItem['quantity'] }} pcs/pax/pckg
-                            @endif
+    @foreach ($cart as $categoryName => $categoryItems)
+        <div>
+            <x-mary-header title="{{ ucwords($categoryName) }}"
+                class="!my-2"
+                size="text-xl"
+                separator />
+            @foreach ($categoryItems as $key => $categoryItem)
+                <x-mary-list-item :item="$categoryItem"
+                    no-separator
+                    no-hover>
+                    <x-slot:avatar>
+                        <x-mary-avatar :image="asset('images/placeholder.jpg')" />
+                    </x-slot:avatar>
+                    <x-slot:value>
+                        {{ $categoryName == 'foods' ? $categoryItem['orderItem']->foodDetail->name . ' - ' . $categoryItem['orderItem']->servingType->name : $categoryItem['orderItem']->name }}
+                    </x-slot:value>
+                    <x-slot:sub-value>
+                        @if ($categoryName == 'foods')
+                            PHP {{ $categoryItem['price'] }} for
+                            {{ $categoryItem['quantity'] }} pax
+                        @else
+                            PHP {{ $categoryItem['price'] }} for
+                            {{ $categoryItem['quantity'] }} pcs/pax/pckg
+                        @endif
 
-                        </x-slot:sub-value>
-                    </x-mary-list-item>
-                @endforeach
-            </div>
-        @endforeach
-        <x-mary-header title="Total: {{ $totalAmount }}"
-            class="!my-2"
-            separator />
-        <x-mary-header title="Customer Information"
-            class="!my-2"
-            separator />
+                    </x-slot:sub-value>
+                </x-mary-list-item>
+            @endforeach
+        </div>
+    @endforeach
+    <x-mary-header title="Total: {{ $totalAmount }}"
+        class="!my-2"
+        separator />
+    <x-mary-header title="Customer Information"
+        class="!my-2"
+        separator />
 
-        <form>
-            <x-input-label for="startDateTime"
-                :value="__('Start')" />
-            <x-text-input wire:model.live="startDateTime"
-                id="startDateTime"
-                class="block mt-1 w-full"
-                type="datetime-local"
-                name="startDateTime"
-                required />
-            <x-input-error :messages="$errors->get('startDateTime')"
-                class="mt-2" />
-            <x-input-label for="endDateTime"
-                :value="__('End')" />
-            <x-text-input wire:model.live="endDateTime"
-                id="endDateTime"
-                class="block mt-1 w-full"
-                type="datetime-local"
-                name="endDateTime"
-                required />
-            <x-input-error :messages="$errors->get('endDateTime')"
-                class="mt-2" />
-            <x-input-label for="location"
-                :value="__('Location')" />
-            <x-text-input wire:model.live="location"
-                id="location"
-                class="block mt-1 w-full mb-4"
-                type="text"
-                name="location"
-                required />
-            <x-input-error :messages="$errors->get('location')"
-                class="mt-2" />
+    <form wire:submit='pay'>
+        <x-input-label for="startDateTime"
+            :value="__('Start')" />
+        <x-text-input wire:model.live="startDateTime"
+            id="startDateTime"
+            class="block mt-1 w-full"
+            type="datetime-local"
+            name="startDateTime"
+            required />
+        <x-input-error :messages="$errors->get('startDateTime')"
+            class="mt-2" />
+        <x-input-label for="endDateTime"
+            :value="__('End')" />
+        <x-text-input wire:model.live="endDateTime"
+            id="endDateTime"
+            class="block mt-1 w-full"
+            type="datetime-local"
+            name="endDateTime"
+            required />
+        <x-input-error :messages="$errors->get('endDateTime')"
+            class="mt-2" />
+        <x-input-label for="location"
+            :value="__('Location')" />
+        <x-text-input wire:model.live="location"
+            id="location"
+            class="block mt-1 w-full mb-4"
+            type="text"
+            name="location"
+            required />
+        <x-input-error :messages="$errors->get('location')"
+            class="mt-2" />
 
-            <x-mary-textarea label="Remarks"
-                wire:model="remarks"
-                placeholder="Notes for the Caterer"
-                rows="5"
-                inline />
+        <x-mary-textarea label="Remarks"
+            wire:model="remarks"
+            placeholder="Notes for the Caterer"
+            rows="5"
+            inline />
 
-        </form>
         <x-mary-button type="submit"
-            label="Proceed to Checkout"
+            label="Proceed to Payment"
             class="btn-primary"
             spinner />
-
-        <h1>Discount</h1>
-        <h1>Pay 70%</h1>
     </form>
 </div>
