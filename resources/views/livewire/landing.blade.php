fd68872a34cca13de364f9faaecabf974209354d
jaytalla <jay.ray.talla06102000@gmail.com>
2024-10-10 16:04:13 +0800

    added design to landing page

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,10 +1,29 @@
-<div class="">
-    <x-mary-header title="CSORS"
-        class="mb-6"
-        subtitle="Catering Service Ordering and Reservation System"
-        separator />
-
-    <a href="{{ route('caterers') }}"><x-primary-button>{{ __('Browse Caterers') }}</x-primary-button></a>
-
-    <h1>Add a touch of simple landing page design</h1>
+<div  class="">
+    {{--
+    <x-mary-header title="CSORS" class="mb-6" subtitle="Catering Service Ordering and Reservation System" separator /> --}}
+    
+    
+    
+    {{-- <h1>Add a touch of simple landing page design</h1> --}}
+    <div style="background-image: url('{{ asset('images/bgs/landing-min.jpg') }}');" 
+        class="absolute top-0 left-0 w-full h-screen bg-black bg-center bg-cover">
+        {{-- OVERLAY  --}}
+        <div class="absolute top-0 left-0 w-full h-screen text-white bg-black/70 ">
+            {{-- MAIN CONTAINER - WIDTH CONSTRAINT  --}}
+            <div class="flex flex-col items-center justify-center h-full py-6 mx-auto md:items-start max-w-7xl sm:px-2 lg:px-4">
+                {{-- CONTENT --}}
+                <div class="space-y-2 w-[90%] sm:w-[70%] px-0 md:px-16 md:w-[70%] lg:w-[50] ">
+                    <h1 class="mb-4">Welcome to CSORS</h1>
+                    <h4>Delicious Catering Options for Every Occasion</h4>
+                    <p>At CSORS, we believe that great food brings people together. Whether you're planning a wedding,
+                        corporate event, birthday party, or any special gathering, we have the perfect catering solutions tailored just
+                        for you.</p>
+                    
+                    <a href="{{ route('caterers') }}" >
+                        <x-primary-button class="mt-4 btn-primary">{{ __('Browse Caterers') }}</x-primary-button>
+                    </a>
+                </div>    
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -6,7 +6,7 @@
 @endphp
 
 <nav x-data="{ open: false }"
-    class="border-b border-gray-100 bg-jt-primary-dark dark:bg-gray-800 dark:border-gray-700">
+    class="fixed z-50 w-full border-b border-gray-100 bg-jt-primary dark:bg-gray-800 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -159,19 +159,19 @@
             <!-- Hamburger -->
             <div class="flex items-center -me-2 sm:hidden">
                 <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400">
+                    class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md focus:outline-none ">
                     <svg class="w-6 h-6"
                         stroke="currentColor"
                         fill="none"
                         viewBox="0 0 24 24">
                         <path :class="{ 'hidden': open, 'inline-flex': !open }"
-                            class="inline-flex"
+                            class="inline-flex text-white"
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{ 'hidden': !open, 'inline-flex': open }"
-                            class="hidden"
+                            class="hidden text-white"
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             stroke-width="2"
