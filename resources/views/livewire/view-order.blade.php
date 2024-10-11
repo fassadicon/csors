262aff68b6d63f6c14a1353c986cce95850f4828
jaytalla <jay.ray.talla06102000@gmail.com>
2024-10-11 21:58:37 +0800

    fixed design in view-order

--- resources/views/livewire/view-order.blade.php
+++ resources/views/livewire/view-order.blade.php
@@ -1,18 +1,20 @@
-<div>
-    <x-mary-header title="Order Information"
-        subtitle="# {{ $order->id }} - PHP {{ $order->total_amount }}"
-        class="!my-2">
-        <x-slot:actions>
-            <x-mary-badge value="Payment: {{ $order->payment_status->getLabel() }}"
-                class="badge-{{ $order->payment_status->getMaryColor() }}" />
-            <x-mary-badge value="Order: {{ $order->order_status->getLabel() }}"
-                class="badge-{{ $order->order_status->getMaryColor() }}" />
-            <a href="{{ route('order-history') }}">
-                <x-mary-button label="Back to Order History"
-                    class="btn-outline" />
-            </a>
-        </x-slot:actions>
-    </x-mary-header>
+<div class="p-4 bg-jt-white rounded-md">
+    <div class="flex justify-between px-4">
+        <x-mary-header title="Order Information" {{-- subtitle="# {{ $order->id }} - PHP {{ $order->total_amount }}" --}}
+            class="!my-4 ">
+        </x-mary-header>
+        
+        <div class="mt-4 flex-col flex justify-end gap-y-4">
+            <div class="flex flex-row gap-x-2 justify-end">
+                <x-mary-badge value="Payment: {{ $order->payment_status->getLabel() }}"
+                    class="badge-{{ $order->payment_status->getMaryColor() }}" />
+                <x-mary-badge value="Order: {{ $order->order_status->getLabel() }}"
+                    class="badge-{{ $order->order_status->getMaryColor() }}" />
+            </div>
+            <h3>#{{ $order->id }} - Total Php {{ number_format($order->total_amount, 2) }}</h3>
+        </div>
+    </div>
+
     @foreach ($order->orderItems as $orderItem)
         <div>
             <x-mary-list-item :item="$orderItem"
@@ -45,16 +47,19 @@
         class="!my-2"
         separator /> --}}
     <x-mary-header title="Customer Information"
-        class="!my-2"
+        class="!my-2" subtitle="Customer: {{ $order->user->name }}"
         separator />
+    <p class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        
     <p class="text-lg font-medium text-gray-900 dark:text-gray-100">
         Caterer: {{ $order->caterer->name }}
     </p>
     <p class="text-lg font-medium text-gray-900 dark:text-gray-100">
-        Start: {{ $order->start }}
+        Start: {{ $order->start->format('M j, Y - g:ia') }} 
+        {{-- - {{ $order->start->diffForHumans() --}}
     </p>
     <p class="text-lg font-medium text-gray-900 dark:text-gray-100">
-        End: {{ $order->end }}
+        End: {{ $order->end->format('M j, Y - g:ia') }}
     </p>
     <p class="text-lg font-medium text-gray-900 dark:text-gray-100">
         Location: {{ $order->location }}
@@ -63,7 +68,7 @@
         Remarks: {{ $order->remarks }}
     </p>
     <p class="text-lg font-medium text-gray-900 dark:text-gray-100">
-        Date Ordered: {{ $order->created_at }}
+        Date Ordered: {{ $order->created_at->format('M j, Y - g:ia') }}
     </p>
 
     @if ($order->cancellationRequest)
@@ -102,17 +107,23 @@
 
     @endif
 
-    @if ($order->payment_status->value == 'pending')
-        <x-primary-button wire:click='payPartial'>{{ __('Pay Partial') }}</x-primary-button>
-        <x-primary-button wire:click='payFull'>{{ __('Pay Full') }}</x-primary-button>
-    @elseif ($order->payment_status->value == 'partial')
-        <x-primary-button wire:click='payRemaining'>{{ __('Pay Remaining Balance') }}</x-primary-button>
-    @endif
-
-    @unless ($order->cancellationRequest)
-        @if ($canRequestCancellation)
-            <x-danger-button wire:click='cancel'>{{ __('Request to Cancel') }}</x-danger-button>
-        @endif
-    @endunless
-
+    <div class="flex flex-col gap-y-2 p-4 md:p-0 w-[90%] md:w-[45%] mt-4">
+        <div class="flex gap-x-2">
+            @if ($order->payment_status->value == 'pending')
+            <x-primary-button class="w-full bg-jt-primary-dark flex !justify-center !text-center" wire:click='payPartial'>{{ __('Pay Partial') }}</x-primary-button>
+            <x-primary-button class="w-full btn-primary flex !justify-center" wire:click='payFull'>{{ __('Pay Full') }}</x-primary-button>
+            @elseif ($order->payment_status->value == 'partial')
+            <x-primary-button class="w-full btn-primary flex !justify-center" wire:click='payRemaining'>{{ __('Pay Remaining Balance') }}</x-primary-button>
+            @endif
+        </div>
+        {{-- CANCEL  --}}
+        @unless ($order->cancellationRequest)
+            @if ($canRequestCancellation)
+                <x-danger-button class="flex justify-center bg-slate-900" wire:click='cancel'>{{ __('Request to Cancel') }}</x-danger-button>
+            @endif
+        @endunless
+        <a href="{{ route('order-history') }}">
+            <x-mary-button label="Back to Order History" class=" w-full py-2 btn-outline" />
+        </a>
+    </div>
 </div>
