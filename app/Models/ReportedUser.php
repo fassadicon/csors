440058fdf70556354cc4f7cd9504eaca3566c4e1
Jay Ray Talla <92903997+jaytalla@users.noreply.github.com>
2024-10-14 03:33:41 +0800

    added view reported-user in caterer and superadmin

--- app/Models/ReportedUser.php
+++ app/Models/ReportedUser.php
@@ -4,9 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ReportedUser extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function caterer(): HasOne
+    {
+        return $this->hasOne(Caterer::class);
+    }
+
+    public function reporter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Relationship to the reported user
+    public function reportedUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'reported_user');
+    }
 }
