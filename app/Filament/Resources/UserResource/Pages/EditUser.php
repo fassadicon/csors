d6bcc5f7f858d67278af2cd0ab2128fe8f7b1a39
snarf <fa.sadicon@gmail.com>
2024-10-24 19:15:36 +0800

    added to edit user for the meantime to notify caterer user

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Illuminate\Database\Eloquent\Model;
+use App\Filament\Resources\UserResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
@@ -15,6 +18,15 @@ class EditUser extends EditRecord
         return [
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
+            Actions\Action::make('Notify to Reupload Requirements')
+                ->action(function (Model $record) {
+                    $recipient = User::find($record->id);
+
+                    $notification = 'Requirements do not pass the verification. Please reupload the correct and updated requirements. Contact the superadmin for more information';
+                    Notification::make()
+                        ->title($notification)
+                        ->sendToDatabase($recipient);
+                }),
         ];
     }
 }
