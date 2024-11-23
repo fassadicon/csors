f065fdf097cd4f7ef409ce3895f8935d99a71c41
fassadicon <fa.sadicon@gmail.com>
2024-11-23 20:31:11 +0800

    Draft print receipt via view receipt modal

--- resources/views/filament/order/receipt.blade.php
+++ resources/views/filament/order/receipt.blade.php
@@ -1,6 +1,18 @@
 <script src="https://cdn.tailwindcss.com"></script>
-<div class="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
-    <h2 class="mb-4 text-xl font-semibold text-gray-900 dark:text-white">Receipt Details</h2>
+<div x-data="{
+    printDiv() {
+        var printContents = this.$refs.container.innerHTML;
+        var originalContents = document.body.innerHTML;
+        document.body.innerHTML = printContents;
+        window.print();
+        document.body.innerHTML = originalContents;
+    }
+}"
+    x-cloak
+    x-ref="container"
+    class="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800"
+    id="receipt-content">
+    <h2 class="mb-4 text-xl font-semibold text-gray-900 dark:text-white">Order #{{ $id }}</h2>
     <table class="w-full mb-6 text-left border-collapse table-auto">
         <thead>
             <tr class="bg-gray-200 dark:bg-gray-700">
@@ -10,32 +22,37 @@
         </thead>
         <tbody>
             @php
-            use Illuminate\Support\Str;
-            $total = 0; // Initialize total variable
+                use Illuminate\Support\Str;
+                $total = 0; // Initialize total variable
             @endphp
             @foreach ($order as $item)
-            <tr class="transition duration-200 hover:bg-gray-100 dark:hover:bg-gray-600">
-                <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">
+                <tr class="transition duration-200 hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">
+                        @php
+                            $itemName = '';
+                            if ($item->orderable) {
+                                if (get_class($item->orderable) === 'App\Models\Food' && $item->orderable->foodDetail) {
+                                    $itemName =
+                                        $item->orderable->foodDetail->name .
+                                        ' - ' .
+                                        $item->orderable->servingType->name .
+                                        ' (' .
+                                        $item->quantity .
+                                        ' pax)';
+                                } elseif ($item->orderable) {
+                                    $itemName = $item->orderable->name . ' (' . $item->quantity . ' set/pcs)';
+                                }
+                            }
+                        @endphp
+                        {{ $itemName ?: 'Item not available' }}
+                    </td>
+                    <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">
+                        ₱{{ number_format($item->amount, 2) }}
+                    </td>
                     @php
-                    $itemName = '';
-                    if ($item->orderable) {
-                    if (get_class($item->orderable) === 'App\Models\Food' && $item->orderable->foodDetail) {
-                    $itemName = $item->orderable->foodDetail->name . ' - ' . $item->orderable->servingType->name . ' ('
-                    . $item->quantity . ' pax)';
-                    } elseif ($item->orderable) {
-                    $itemName = $item->orderable->name . ' (' . $item->quantity . ' set/pcs)';
-                    }
-                    }
+                        $total += $item->amount; // Add item price to total
                     @endphp
-                    {{ $itemName ?: 'Item not available' }}
-                </td>
-                <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">
-                    ₱{{ number_format($item->amount, 2) }}
-                </td>
-                @php
-                $total += $item->amount; // Add item price to total
-                @endphp
-            </tr>
+                </tr>
             @endforeach
             <tr class="font-bold bg-gray-200 dark:bg-gray-700">
                 <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">Total</td>
@@ -56,12 +73,13 @@
         </thead>
         <tbody>
             @php
-            $totalPayments = 0; // Initialize total payments variable
+                $totalPayments = 0; // Initialize total payments variable
             @endphp
-            @if (count($payments) <= 0) <td class="self-center w-full p-2 text-gray-800 dark:text-gray-200">No payments
-                yet.</td>
-                @endif
-                @foreach ($payments as $payment)
+            @if (count($payments) <= 0)
+                <td class="self-center w-full p-2 text-gray-800 dark:text-gray-200">No payments
+                    yet.</td>
+            @endif
+            @foreach ($payments as $payment)
                 <tr class="transition duration-200 hover:bg-gray-100 dark:hover:bg-gray-600">
                     <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">
                         {{ Str::camel($payment->method) ?: Str::camel($payment->type) }}
@@ -73,29 +91,36 @@
                         {{ $payment->created_at->format('Y-m-d H:i') }}
                     </td>
                     @php
-                    $totalPayments += $payment->amount; // Add payment amount to total payments
+                        $totalPayments += $payment->amount; // Add payment amount to total payments
                     @endphp
                 </tr>
-                @endforeach
-                <tr class="font-bold bg-gray-200 dark:bg-gray-700">
-                    <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">Total Payments</td>
-                    <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">₱{{ number_format($totalPayments, 2)
-                        }}</td>
-                    <td class="px-4 py-2 border-b"></td> <!-- Empty cell for date -->
-                </tr>
+            @endforeach
+            <tr class="font-bold bg-gray-200 dark:bg-gray-700">
+                <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">Total Payments</td>
+                <td class="px-4 py-2 text-gray-800 border-b dark:text-gray-200">₱{{ number_format($totalPayments, 2) }}
+                </td>
+                <td class="px-4 py-2 border-b"></td> <!-- Empty cell for date -->
+            </tr>
         </tbody>
     </table>
     @php
-    $balance = $total - $totalPayments; // Calculate balance
+        $balance = $total - $totalPayments; // Calculate balance
     @endphp
     @if ($balance > 0)
-    <div class="p-4 mt-4 !text-red-700 bg-red-100 border border-red-400 rounded dark:bg-red-800 dark:text-red-200">
-        <strong>Insufficient Payment!</strong> You still owe ₱{{ number_format($balance, 2) }}.
-    </div>
+        <div class="p-4 mt-4 !text-red-700 bg-red-100 border border-red-400 rounded dark:bg-red-800 dark:text-red-200">
+            <strong>Insufficient Payment!</strong> You still owe ₱{{ number_format($balance, 2) }}.
+        </div>
     @else
-    <div
-        class="p-4 mt-4 text-green-700 bg-green-100 border border-green-400 rounded dark:bg-green-800 dark:text-green-200">
-        <strong>Payment Complete!</strong> You have paid in full.
-    </div>
+        <div
+            class="p-4 mt-4 text-green-700 bg-green-100 border border-green-400 rounded dark:bg-green-800 dark:text-green-200">
+            <strong>Payment Complete!</strong> You have paid in full.
+        </div>
     @endif
+
+    <div class="mt-6 flex justify-center">
+        <button x-on:click="printDiv()"
+            class="px-6 py-3 text-sm font-medium text-dark dark:text-white bg-blue-500 rounded-lg shadow-md transition-all duration-200 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-500">
+            Print Receipt
+        </button>
+    </div>
 </div>
